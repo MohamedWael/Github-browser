1a501f702c31cfc2b8286697415781a7c6fdf7d7
Mohamed Wael <mohamed.wael@v-apps.co>
2019-09-08 01:28:58 +0200

    initializing storage manager

--- app/src/main/java/io/thed/applevel/application/App.kt
+++ app/src/main/java/io/thed/applevel/application/App.kt
@@ -3,11 +3,13 @@ package io.thed.applevel.application
 import android.app.Application
 import io.thed.applevel.BASE_API
 import io.thed.applevel.network.Retrofit
+import io.thed.applevel.storage.StorageManager
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Retrofit.init(BASE_API)
+        StorageManager.init(this)
     }
 }
