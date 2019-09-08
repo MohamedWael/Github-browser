56bc60852e5eccafd40bcc419b130ba1d85f9c4d
Mohamed Wael <mohamed.wael@v-apps.co>
2019-09-08 20:49:53 +0200

    generalizing network service to be replaceable when testing

--- app/src/main/java/io/thed/applevel/network/networkservice/NetworkServiceImpl.kt
+++ app/src/main/java/io/thed/applevel/network/networkservice/NetworkServiceImpl.kt
@@ -1,13 +1,17 @@
-package io.thed.applevel.network
+package io.thed.applevel.network.networkservice
 
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import io.thed.applevel.network.ErrorHandler
+import io.thed.applevel.network.ErrorHandlerImpl
+import io.thed.applevel.network.RestCleint
+import io.thed.applevel.network.Retrofit
 
-open class NetworkService<Response, RestClient> {
+open class NetworkServiceImpl<Response, RestClient> : NetworkService<Response, RestClient> {
 
-    fun execute(
+    override fun execute(
         observable: Observable<Response>,
         responseCallback: (Response) -> Unit,
         errorCallback: (ErrorHandler) -> Unit
@@ -19,13 +23,13 @@ open class NetworkService<Response, RestClient> {
         }, { errorCallback(ErrorHandlerImpl(it)) })
 
 
-    fun createRestClient(tClass: Class<RestClient>): RestClient {
+    override fun createRestClient(tClass: Class<RestClient>): RestClient {
         return Retrofit.createClient(tClass)
     }
 }
 
 fun <Response> network(service: NetworkService<Response, RestCleint>.() -> Unit): NetworkService<Response, RestCleint> {
-    val networkService = NetworkService<Response, RestCleint>()
+    val networkService = NetworkServiceImpl<Response, RestCleint>()
     service(networkService)
     return networkService
 }
